fc52d1a1bffa994750276035767df4b858970f83
Connor Ferguson <cpfergus1@gmail.com>
2020-08-22 21:01:49 -0600

    Minor ordering and naming changes

--- test/Card_Generator_test.rb
+++ test/Card_Generator_test.rb
@@ -13,14 +13,6 @@ class TestCard_Generator <Minitest::Test
     assert_instance_of Card_Generator, @generate
   end
 
-  def test_vaidate_cards
-    card = ["Jack", :heart, 11]
-    assert_equal false, @generate.validate_card(card)
-
-    card = ["Freddy Mercury", :ear_drum_slayer, 999]
-    assert_equal true, @generate.validate_card(card)
-  end
-
   def test_create_a_card
     @generate = Card_Generator.new("./test/cards_test.txt").cards
     card0= @generate[0]
@@ -32,7 +24,15 @@ class TestCard_Generator <Minitest::Test
     assert_equal "Queen", name
   end
 
-  def test_create_cards
+  def test_vaidate_cards
+    card = ["Jack", :heart, 11]
+    assert_equal false, @generate.validate_card(card)
+
+    card = ["Freddy Mercury", :ear_drum_slayer, 999]
+    assert_equal true, @generate.validate_card(card)
+  end
+
+  def test_create_deck
     assert_equal 52 , @generate.cards.length
   end
 end
